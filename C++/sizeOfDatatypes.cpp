6c2fea678722cd77752c4f46674e0251227fd438
Poorva G Chapolkar <poorvachapolkar1@gmail.com>
2022-10-31 11:23:50 +0530

    Added a C++ program that displays the size of the basic datatypes in the current working environment

---
+++ C++/sizeOfDatatypes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout<<"In the current environment: \nThe size of Int is "<<sizeof(int)<<"\nThe size of short is "<<sizeof(short)<<"\nThe size of long is "<<sizeof(long)<<"\nThe size of float is "<<sizeof(float)<<"\nThe size of double is "<<sizeof(double)<<"\nThe size of bool is "<<sizeof(bool)<<"\n";
+
+	return 0;
+
+
+}
