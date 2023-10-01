70684fdd3137beb8fe0817a6db813020e7d60e8b
bhoomikarao22 <95897461+bhoomikarao22@users.noreply.github.com>
2023-10-01 22:37:39 +0530

    Update Factorial.py
    Addition of user input to calculate the factorial

--- Python/Factorial.py
+++ Python/Factorial.py
@@ -1,4 +1,15 @@
 def fact(num):
-    return num if num == 1 else num * fact(num - 1)
+    if num == 0 or num == 1:
+        return 1
+    return num * fact(num - 1)
 
-print(fact(5))
+while True:
+    user_input = input("Enter a non-negative integer to calculate its factorial: ")
+    
+    if user_input.isdigit():
+        n = int(user_input)
+        print(f"The factorial of {n} is {fact(n)}.")
+        break
+    else:
+        print("Invalid input. Please enter a non-negative integer.")
+
