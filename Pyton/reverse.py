85001427145f7706c3563755084ebb8d75c4d89b
Gautham Shankar <81378777+GauthamShankar21@users.noreply.github.com>
2023-10-01 09:57:10 +0530

    Create reverse.py

---
+++ Pyton/reverse.py
@@ -0,0 +1,9 @@
+import array
+ 
+#The original array
+new_arr=array.array('i',[2,4,6,8,10,12])
+print("Original Array is :",new_arr)
+ 
+#reversing using reverse()
+new_arr.reverse()
+print("Reversed Array:",new_arr)
