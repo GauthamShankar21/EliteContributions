d0ba12987fd650089d157c9ef6923c6c0c01bbb5
Gautham Shankar <81378777+GauthamShankar21@users.noreply.github.com>
2023-10-01 09:58:55 +0530

    Create flipud.py

---
+++ Python/flipud.py
@@ -0,0 +1,9 @@
+import numpy as np
+ 
+#The original NumPy array
+new_arr=np.array(['A','s','k','P','y','t','h','o','n'])
+print("Original Array is :",new_arr)
+ 
+#reversing using flipud() Method
+res_arr=np.flipud(new_arr)
+print("Resultant Reversed Array:",res_arr)
