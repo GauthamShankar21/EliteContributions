6f952bd9036a3dba022f2c6378171fe1940a89e3
Sanvi <107338001+sanvi-05@users.noreply.github.com>
2023-10-01 15:02:47 +0530

    Update CheckPrice.java
    I have added an extra condition of checking if the prices are equal.

--- Java/CheckPrice.java
+++ Java/CheckPrice.java
@@ -15,12 +15,17 @@ public class CheckPrice {
             System.out.println("Enter the value of the last product you want to buy: ");
             double price03 = scan.nextDouble();
 
-            if ((price01 < price02) && (price01 < price03)) {
-                System.out.println("The first product is cheaper:  " + price01);
-            } else if ((price02 < price01) && (price02 < price03)) {
-                System.out.println("The second product is cheaper:  " + price02);
-            } else if ((price03 < price01) && (price03 < price02)) {
-                System.out.println("The third product is cheaper:  " + price03);
+            if((price01==price02) && (price02==price03)){
+                    System.out.println("All products have the same price:  " + price01);
+            }
+            else{
+                    if ((price01 < price02) && (price01 < price03)) {
+                        System.out.println("The first product is cheaper:  " + price01);
+                    } else if ((price02 < price01) && (price02 < price03)) {
+                        System.out.println("The second product is cheaper:  " + price02);
+                    } else if ((price03 < price01) && (price03 < price02)) {
+                        System.out.println("The third product is cheaper:  " + price03);
+                    }
             }
         }
 }
