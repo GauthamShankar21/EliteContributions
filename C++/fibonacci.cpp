a462fb3090cf33639821e6c83978fab1f3d3e42f
vansh tyagi <113906488+vanshtyagi38@users.noreply.github.com>
2023-10-11 20:03:31 +0530

    Update fibonacci.cpp
    In this version:
    
    We directly calculate the Fibonacci numbers from the bottom up without the need for recursive function calls.
    We use an iterative loop to build the Fibonacci sequence, starting from 0 and 1.
    
    The dynamic programming approach eliminates the need for the helper function and memoization array, making the code more effective and efficient.

--- C++/fibonacci.cpp
+++ C++/fibonacci.cpp
@@ -1,17 +1,17 @@
 class Solution {
-    int helper(int n, vector<int>& dp)
-    {
-        if(n==0 || n==1) return n;
-        else if(dp[n]!=-1) return dp[n];
-        else 
-        {
-        dp[n]=helper(n-1, dp)+helper(n-2, dp);
-        return dp[n];
-        }
-    }
 public:
     int fib(int n) {
-        vector<int> dp(n+1,-1);
-        return helper(n, dp);
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        
+        vector<int> dp(n + 1, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n];
     }
 };
