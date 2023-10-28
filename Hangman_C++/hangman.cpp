bc551e33007384cf8e91744eb2e08275ffea87a4
Purna Shrestha <107195487+purnasth@users.noreply.github.com>
2023-10-28 11:01:44 +0545

    Update hangman.cpp with the ASCII art

--- Hangman_C++/hangman.cpp
+++ Hangman_C++/hangman.cpp
@@ -1,82 +1,120 @@
 // Code by Purna Shrestha https://github.com/purnasth
 
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
+#include <set>
 #include <ctime>
 #include <cstdlib>
 
 using namespace std;
 
-struct Word {
+struct Word
+{
     string word;
     string hint;
 };
 
 vector<Word> wordList = {
     {"python", "A popular programming language"},
-    {"java", "Another widely used programming language"},
+    {"javascript", "Another widely used programming language"},
     {"programming", "The act of writing code"},
     {"hangman", "The name of this game"},
     {"computer", "An electronic device for processing data"},
     {"keyboard", "Input device for computers"},
-    {"developer", "Someone who writes software"}
-};
+    {"developer", "Someone who writes software"}};
 
-Word chooseWord() {
+vector<string> stages = {
+    "  +---+\n  |   |\n      |\n      |\n      |\n      |\n========",
+    "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n========",
+    "  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
+    "  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n========",
+    "  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n========",
+    "  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n========"};
+
+Word chooseWord()
+{
     srand(time(0));
     int randomIndex = rand() % wordList.size();
     return wordList[randomIndex];
 }
 
-void playHangman(const string& word, const string& hint) {
-    string guessedLetters;
-    int attempts = 6;
+void playHangman(const string &word, const string &hint)
+{
+    string lowerWord = word;
+    for (char &c : lowerWord)
+    {
+        c = tolower(c);
+    }
+
+    set<char> guessedLetters;
+    int attempts = 5;
 
     cout << "Welcome to Hangman!" << endl;
     cout << "Hint: " << hint << endl;
-    cout << "Try to guess the word. You have 6 attempts." << endl;
+    cout << "Try to guess the word. You have 5 attempts." << endl;
 
-    while (attempts > 0) {
+    while (attempts > 0)
+    {
         string displayWord;
-        for (char letter : word) {
-            if (guessedLetters.find(letter) != string::npos) {
+        for (char letter : lowerWord)
+        {
+            if (guessedLetters.find(letter) != guessedLetters.end())
+            {
                 displayWord += letter;
-            } else {
+            }
+            else
+            {
                 displayWord += "_";
             }
         }
 
+        cout << stages[5 - attempts] << endl;
         cout << "Word: " << displayWord << endl;
-        cout << "Guessed letters: " << guessedLetters << endl;
+        cout << "Guessed letters: ";
+        for (char letter : guessedLetters)
+        {
+            cout << letter << " ";
+        }
+        cout << endl;
         cout << "Attempts left: " << attempts << endl;
+
+        if (set<char>(lowerWord.begin(), lowerWord.end()) == guessedLetters)
+        {
+            cout << "Congratulations! You guessed the word: " << word << endl;
+            return;
+        }
 
         char guess;
         cout << "Guess a letter: ";
         cin >> guess;
+        guess = tolower(guess);
 
-        if (guessedLetters.find(guess) != string::npos) {
+        if (guessedLetters.find(guess) != guessedLetters.end())
+        {
             cout << "You've already guessed that letter." << endl;
-        } else if (word.find(guess) != string::npos) {
-            guessedLetters += guess;
-        } else {
-            guessedLetters += guess;
-            attempts--;
         }
-
-        if (word.find_first_not_of(guessedLetters) == string::npos) {
-            cout << "Congratulations! You guessed the word: " << word << endl;
-            break;
+        else
+        {
+            guessedLetters.insert(guess);
+            if (lowerWord.find(guess) == string::npos)
+            {
+                attempts--;
+            }
         }
     }
 
-    if (attempts == 0) {
+    if (attempts == 0)
+    {
         cout << "Out of attempts! The word was '" << word << "'." << endl;
+        cout << stages[5] << endl;
     }
 }
 
-int main() {
+int main()
+{
     Word chosenWord = chooseWord();
     playHangman(chosenWord.word, chosenWord.hint);
     return 0;
 }
+
